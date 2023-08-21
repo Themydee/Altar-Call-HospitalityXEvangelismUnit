642b9f1f35418721bac204e98236b59d26c89999
Temidayo <nifetemiboy@gmail.com>
2023-08-21 17:17:26 +0100

    new

--- server/index.js
+++ server/index.js
@@ -5,11 +5,24 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-import nodemon from "nodemon";
+import convertsRoutes from "./routes/converts.js";
+import breakdownRoutes from "./routes/breakdown.js";
+import managementRoutes from "./routes/management.js";
+import generalRoutes from "./routes/general.js";
 
 /* Configurations */
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}))
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+
+/* ROUTES */
+app.use("/converts", converts);
+app.use("/breakdown", breakdown);
+app.use("/management", management);
+app.use("/general", general)
